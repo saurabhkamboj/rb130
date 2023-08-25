85f50beb4f3ef0b2ae387540ef6f4755480a4409
Saurabh Kamboj <skamboj97@gmail.com>
2023-08-25 12:42:14 +0530

    Add file & modify file

--- lesson_2/minitest_practice/car_spec.rb
+++ lesson_2/minitest_practice/car_spec.rb
@@ -5,6 +5,6 @@ require_relative 'car'
 describe 'Car#wheels' do
   it 'has 4 wheels' do
     car = Car.new
-    _(car.wheels).must_equal 4           # this is the expectation
+    _(car.wheels).must_equal 4 # this is the expectation
   end
 end
